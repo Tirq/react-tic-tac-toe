dd909f1a9df1cb8f9acf7ae6b0ec1b5285b28fed
Tirq <tiagogelatti@outlook.com>
2019-03-16 11:55:18 +0100

    Updates

--- precache-manifest.28c879b659a3a9060c457fc0bf281389.js
+++ precache-manifest.28c879b659a3a9060c457fc0bf281389.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-tic-tac-toe/static/js/runtime~main.7d1b9009.js"
   },
   {
-    "revision": "1a2ebeb727090fa3d033",
-    "url": "/react-tic-tac-toe/static/js/main.1a2ebeb7.chunk.js"
+    "revision": "4ce6a70d7b11a99a9c9f",
+    "url": "/react-tic-tac-toe/static/js/main.4ce6a70d.chunk.js"
   },
   {
     "revision": "ea089c583e222e215502",
     "url": "/react-tic-tac-toe/static/js/2.ea089c58.chunk.js"
   },
   {
-    "revision": "1a2ebeb727090fa3d033",
+    "revision": "4ce6a70d7b11a99a9c9f",
     "url": "/react-tic-tac-toe/static/css/main.032ae7b2.chunk.css"
   },
   {
-    "revision": "0e46d8db33ebb73a6ded6b999faefd68",
+    "revision": "1e3c63b5c11fa31e9fccbe22572ca0ff",
     "url": "/react-tic-tac-toe/index.html"
   }
 ];
