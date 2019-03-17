70ed94c571cd9fb42c9d3ddfcf986c1a9cf312a0
Tirq <tiagogelatti@outlook.com>
2019-03-17 17:44:28 +0100

    Updates

--- precache-manifest.ae2ad00464915ff1ee75a4878411edca.js
+++ precache-manifest.ae2ad00464915ff1ee75a4878411edca.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-tic-tac-toe/static/js/runtime~main.7d1b9009.js"
   },
   {
-    "revision": "46a88f56e05b05f7e42d",
-    "url": "/react-tic-tac-toe/static/js/main.46a88f56.chunk.js"
+    "revision": "61c36105c9006f8453ca",
+    "url": "/react-tic-tac-toe/static/js/main.61c36105.chunk.js"
   },
   {
     "revision": "ea089c583e222e215502",
     "url": "/react-tic-tac-toe/static/js/2.ea089c58.chunk.js"
   },
   {
-    "revision": "46a88f56e05b05f7e42d",
+    "revision": "61c36105c9006f8453ca",
     "url": "/react-tic-tac-toe/static/css/main.aeeb137a.chunk.css"
   },
   {
-    "revision": "f5183e1ee9a875ab4c8274d7ff4b47aa",
+    "revision": "403223436d8d6dde772a3dfe2d2c1edd",
     "url": "/react-tic-tac-toe/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-tic-tac-toe/precache-manifest.add206505ea25d49766dc1aa00a05e81.js"
+  "/react-tic-tac-toe/precache-manifest.ae2ad00464915ff1ee75a4878411edca.js"
 );
 
 workbox.clientsClaim();
